aa85eccf291b58db670a71c89432dde6691b7f40
Aleksandar Milosevic <dev@sasxa.net>
2020-10-08 09:37:42 +0300

    Add Prettier config for svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
-	export let name: string;
+export let name: string;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+  <h1>Hello {name}!</h1>
 </main>
 
 <style lang="scss">
-	main {
-		padding: 2rem;
-		background: pink;
-	}
+main {
+  padding: 2rem;
+}
 
-	h1 {
-		text-align: center;
-	}
+h1 {
+  text-align: center;
+}
 </style>
