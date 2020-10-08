ccaf46f2aac686e0fee0dbf9aa78f607e62da014
Aleksandar Milošević <dev@sasxa.net>
2020-10-08 09:55:39 +0300

    component: perspective hover (#1)
    * feat: Add 3d perspective hover
    
    * Update formatting

---
+++ src/c/card-perspective-example.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+import PerspectiveCard from "./card-perspective-hover.svelte";
+</script>
+
+<PerspectiveCard rotate={5}>
+  <div class="form">
+    <input type="text" id="test" />
+    <label for="test"> testing... </label>
+
+    <button>test</button>
+  </div>
+</PerspectiveCard>
+<PerspectiveCard>
+  <img src="http://unsplash.it/320/180" alt="" />
+</PerspectiveCard>
+
+<style lang="scss">
+:global(.card) {
+  // width: 16rem;
+  height: 9rem;
+  // margin: 1rem;
+  padding: 0.25rem;
+
+  display: flex;
+  transition: box-shadow 200ms;
+  background-color: #ddd;
+
+  &:not(.hover) {
+    transition: box-shadow 200ms, transform 300ms ease;
+  }
+
+  &:hover {
+    box-shadow: 0 0 5px rgba(black, 0.5);
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    border: 1px dashed;
+    padding: 1rem;
+    width: 100%;
+    height: 100%;
+
+    > * {
+      margin-top: 1rem;
+    }
+  }
+  img {
+    width: 100%;
+  }
+}
+</style>
